794785940b6c145bab7af8178a929d737d2a165a
Excel909 <excellentayomide@gmail.com>
2024-07-13 20:18:38 +0100

    second commit with grat progress.. created tabs and side nav

---
+++ scroller.js
@@ -0,0 +1,11 @@
+const sr = ScrollReveal();
+
+// sr.reveal('.box',{
+//     origin: 'left',
+//     distance: '20px',
+//     duration: 1000,
+//     delay:0,
+//     easing:'ease-out',
+//     reset:true
+// }); 
+
